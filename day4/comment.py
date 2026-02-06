3bcc384b3809e38c67f5ebf8cd940802185db86b
sagagouthamsai <sagagouthamsai@gmail.com>
2026-02-06 23:29:36 +0530

    indentation and comments

---
+++ day4/comment.py
@@ -0,0 +1,17 @@
+#coments are used to explain code and make it more readable. They are ignored by the interpreter.
+
+#single line comment
+
+'''
+Docstring for day4.comment
+''' 
+# This is a multi-line comment
+
+"""
+This is also a multi-line comment
+"""
+
+def add(a, b):
+    """This function returns sum of two numbers"""
+    return a + b
+
---
+++ day4/intendation.py
@@ -0,0 +1,44 @@
+#if 5 > 2:
+#print("Five is greater than two")
+
+if 5 > 2:
+    print("Five is greater than two")
+
+#Fix case error
+#Print("Hello World")
+
+print("Hello World")
+
+#if True:
+#	print("Hello")
+#    print("World")
+
+if True:
+	print("Hello")
+print("World")
+
+#x = 10
+#if x = 10:
+#    print("Equal")
+
+x = 10
+if x == 10:
+    print("Equal")
+
+#if 10 > 5
+#    print("Correct")
+
+if 10 > 5:
+    print("Correct")
+
+if True:
+    print("A")
+print("B")
+
+#x = 5
+#if x == 5:
+#print("Correct")
+
+x = 5
+if x == 5:
+    print("Correct")
