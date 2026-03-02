8a4fa012b9b2e01854ee7fe1ede7029e6e399a67
sagagouthamsai <sagagouthamsai@gmail.com>
2026-03-02 23:05:04 +0530

    tuple intermideate

---
+++ day17/tuple_methods.py
@@ -0,0 +1,11 @@
+#Tule only has two built-in methods: count and index
+my_tuple = (1, 2, 3, 4, 5, 1, 2)
+
+#count() method returns the number of times a specified value appears in the tuple
+print(my_tuple.count(1)) 
+print(my_tuple.count(2))
+
+#index() method returns the index of the first occurrence of a specified value in the tuple
+print(my_tuple.index(1))
+print(my_tuple.index(2))
+
