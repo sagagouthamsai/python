8efbe6f9e1dc3902d514e3131226d3c21098fcdd
sagagouthamsai <sagagouthamsai@gmail.com>
2026-03-03 23:48:27 +0530

    tuple intermideate

--- day17/tuple_methods.py
+++ day17/tuple_methods.py
@@ -8,4 +8,10 @@ print(my_tuple.count(2))
 #index() method returns the index of the first occurrence of a specified value in the tuple
 print(my_tuple.index(1))
 print(my_tuple.index(2))
-
+#.
+#finding duplicate values in a tuple using count() method
+my_tuple = (1, 2, 3, 4, 5, 1, 2)
+print("---------------------------------------------------")
+for item in my_tuple:
+    if my_tuple.count(item) > 1:
+        print(my_tuple[item])
