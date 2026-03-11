f58ac1eb39d0a9f56b35571ed4f49e3e17a2eec2
sagagouthamsai <sagagouthamsai@gmail.com>
2026-03-11 21:34:31 +0530

    tuple intermediate problems

--- day19/problems.py
+++ day19/problems.py
@@ -1,22 +1,52 @@
-#Create a tuple with 5 numbers and print the third element.
-mytuple=(1,2,3,5,5)
-#length of the tuple
-print(len(mytuple))
+#Tuple Concatenation
+t1 = (1,2,3)
+t2 = (4,5,6)
+print(t1+t2)
 
-#Tuple Slicing
-tuple1=(10,20,30,40,50,60)
-print(tuple1[:3])
-print(tuple1[-3:])
-print(tuple1[1:4])
+#Tuple Repetition
+tuple1=(7,8)
+print(tuple1*4)
 
-#Check whether 15 exists in the tuple.
-test_tuple=(5,10,15,20,25)
+#Built-in Functions
+t = (5,10,15,20)
+print(max(t))
+print(min(t))
+print(sum(t))
 
-if 15 in test_tuple:
-    print("yes")
-else:
-    print("no")
+#Count Occurrences
+tuple2 = (1,2,2,3,2,4)
+print(tuple2.count(2))
 
-#print all elements
-for char in mytuple:
-    print(char)
+#Find Index
+tuple3 = (10,20,30,40)
+print(tuple3.index(30))
+
+#Try changing the first element of a tuple (10,20,30).
+tuple4=(10,20,30)
+#tuple4[0]=100
+
+#Tuple Packing
+non_packed_tuple=1,2,3,4,5,6
+print(non_packed_tuple)
+
+#Tuple Unpacking
+a,b,c=tuple4
+print(a)
+print(b)
+print(c)
+
+#Swap Using Tuple Unpacking
+num1 = 10
+num2 = 20
+print(num1)
+print(num2)
+num1,num2=num2,num1
+print(num1)
+print(num2)
+
+#Convert List to Tuple to list
+list1=[1,2,3,4]
+converted_tuple=tuple(list1)
+print(converted_tuple)
+reverse_conversion=list(converted_tuple)
+print(reverse_conversion)
