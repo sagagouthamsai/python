9c1f5498e8f89e27a0b59a412d65d9027469c75c
sagagouthamsai <sagagouthamsai@gmail.com>
2026-03-13 19:25:21 +0530

    Added set methods

---
+++ day21/set_methods.py
@@ -0,0 +1,21 @@
+"""
+add()   	                    	Adds an element to the set
+remove()	                    	Removes the specified element
+pop()	                        	Removes an element from the set
+copy()	                        	Returns a copy of the set
+clear()	 	                        Removes all the elements from the set
+discard()	                    	Remove the specified item
+union()	                        |	Return a set containing the union of sets
+update()	                    |=	Update the set with the union of this set and others
+difference()	                -	Returns a set containing the difference between two or more sets
+difference_update()	            -=	Removes the items in this set that are also included in another, specified set
+intersection()	                &	Returns a set, that is the intersection of two other sets
+intersection_update()          	&=	Removes the items in this set that are not present in other, specified set(s)
+symmetric_difference()	        ^	Returns a set with the symmetric differences of two sets
+symmetric_difference_update()	^=	Inserts the symmetric differences from this set and another
+isdisjoint()	                	Returns whether two sets have a intersection or not
+issubset()                  	<=	Returns True if all items of this set is present in another set
+                                <	Returns True if all items of this set is present in another, larger set
+issuperset()	                >=	Returns True if all items of another set is present in this set
+ 	                            >	Returns True if all items of another, smaller set is present in this set
+"""
