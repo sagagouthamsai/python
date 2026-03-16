bedf3393b453b149c4afc575bb4ba9551940eb61
sagagouthamsai <sagagouthamsai@gmail.com>
2026-03-16 23:35:29 +0530

    Added dictionaries basics

---
+++ day23/basic_dict.py
@@ -0,0 +1,16 @@
+#dictionaries are orderd ,mutable and unique 
+#dictionaries are unque in the sence that they follow the structure of key:value pair 
+#they are mutable we can change the elements of dictionaries
+#keys in the dict must and should be unique but value may repeat
+
+
+#creating an dict
+#to create an dictionary we use {}along with key:value pair or we can even use dict() function
+example_dict={"name":"goutham","age":19}
+print(example_dict)
+
+#empty dict
+#we can use both of creation methods to make an empty dict
+empty=dict()
+
+
