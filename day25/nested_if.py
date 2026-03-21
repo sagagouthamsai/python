b0b93a7fc9d32b665090a975924e648b48a15aac
sagagouthamsai <sagagouthamsai@gmail.com>
2026-03-21 21:06:36 +0530

    Added nested if

---
+++ day25/nested_if.py
@@ -0,0 +1,27 @@
+#Nested If:
+#You can have if statements inside if statements. This is called nested if statements.
+
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+
+#Multiple Levels of Nesting
+score = 85
+attendance = 90
+submitted = True
+
+if score >= 60:
+  if attendance >= 80:
+    if submitted:
+      print("Pass with good standing")
+    else:
+      print("Pass but missing assignment")
+  else:
+    print("Pass but low attendance")
+else:
+  print("Fail")
