8ad9b3de0a5ef80f980fe31ddee4967065bfcd65
sagagouthamsai <sagagouthamsai@gmail.com>
2026-03-23 20:41:37 +0530

    Added match statement

---
+++ day26/match_problems1.py
@@ -0,0 +1,18 @@
+#Print Day Name
+day= int(input("enter the day number in week(1-7) : "))
+
+match day:
+    case 1:
+        print("sunday")
+    case 2:
+        print("monday")
+    case 3:
+        print("tuesday")
+    case 4:
+        print("wednesday")
+    case 5:
+        print("thursday")
+    case 6:
+        print("friday")
+    case 7:
+        print("saturday")
