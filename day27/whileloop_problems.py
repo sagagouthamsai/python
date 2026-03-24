a488c2561ed4245976460c887b2b406759cd8a97
sagagouthamsai <sagagouthamsai@gmail.com>
2026-03-24 23:58:41 +0530

    Added while loop basic problems

---
+++ day27/whileloop_problems.py
@@ -0,0 +1,36 @@
+#Print Numbers from 1 to N
+n=5#int(input("enter the n value : "))
+i=1
+while i<=n:
+    print(i)
+    i+=1
+
+#Print Even Numbers
+initial=0
+end_value=20
+while initial<=end_value:
+    print(initial)
+    initial+=2
+
+#Print Numbers in Reverse
+ini=0
+end_value1=10
+while end_value1>ini:
+    print(end_value1)
+    end_value1-=1
+
+#Find the sum of numbers from 1 to n.
+
+i1=0
+i2=0
+n1=10
+while i1<=n1:
+    i1+=i2
+    i2+=1
+print(i1,"abc")
+
+#Multiplication Table
+i=1
+while i<=10:
+    print("3 x",i,"=",3*i)
+    i+=1
