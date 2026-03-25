ee5da4fb81c5cd459ce9ecb72e99606ce2eeede4
sagagouthamsai <sagagouthamsai@gmail.com>
2026-03-25 13:22:38 +0530

    Added leetcode problems

--- day27/leetcode.py
+++ day27/leetcode.py
@@ -18,8 +18,19 @@ print(int(bin_n[::-1],2))
 
 print(int(f"{n:032b}"[::-1],2))
 """
-
+"""
 s="   fly me   to   the moon  "
 split_s=s.split()
 print(split_s)
 print(len(split_s[-1]))
+"""
+
+s = "0P"
+rev_s=s[::-1]
+combined_s=""
+for i in s.lower():
+    if i.isalnum():
+        combined_s+=i
+print(combined_s[::-1])
+print(rev_s)
+print(combined_s)
