b02ee5dbb04ea096d0fe8fd841b6a8e546402db1
sagagouthamsai <sagagouthamsai@gmail.com>
2026-03-26 15:56:31 +0530

    Added for-loop concept

---
+++ day28/for_loop.py
@@ -0,0 +1,19 @@
+#for loop is another kind of loop which is mostly used as it is easy and simple
+
+for i in range(5):
+    print(i)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+for i in range(5):
+    print(i)
+
+for i in range(1, 10, 2):
+    print(i)
+
+for i in range(3):
+    for j in range(2):
+        print(i, j)
+
