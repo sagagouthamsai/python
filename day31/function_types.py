f87b36127092c96735f797aa95fd34561d4fe74e
sagagouthamsai <sagagouthamsai@gmail.com>
2026-04-01 23:01:28 +0530

    Added functions types

---
+++ day31/function_types.py
@@ -0,0 +1,17 @@
+#types of functions
+#no parameters
+def hello():
+    print("Hi")
+
+#with parameters
+def show(num):
+    print(num)
+
+#no parameters,with return
+def get_number():
+    return 10
+
+#parameters with return
+def square(n):
+    return n * n
+
