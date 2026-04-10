c82f3cce492c18d5b17030cfef7ea23aa2319374
sagagouthamsai <sagagouthamsai@gmail.com>
2026-04-10 22:04:54 +0530

    Added decorators introduction

---
+++ day38/decorators.py
@@ -0,0 +1,13 @@
+#decorators are used to modify the behavior of a function without changing its code.
+
+def decorator(func
+            ):
+    def wrapper(a, b):
+        return func(a, b) * 2
+    return wrapper
+
+@decorator
+def add(a, b):
+    return a + b
+
+print(add(1,2))
