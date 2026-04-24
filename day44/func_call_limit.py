0e0a5e6682745fbe5483eba8e80913380301c95d
sagagouthamsai <sagagouthamsai@gmail.com>
2026-04-25 00:29:28 +0530

    Added decorators problem 8

---
+++ day44/func_call_limit.py
@@ -0,0 +1,18 @@
+def limit_calls(func):
+    count=0
+    def count_check(*args,**kwargs):
+        nonlocal count
+        if count>=2:
+            print("Limit reached")
+            return
+        count+=1
+        return func(*args,**kwargs)
+    return count_check
+
+@limit_calls
+def test():
+    print("Running")
+
+test()
+test()
+test()
