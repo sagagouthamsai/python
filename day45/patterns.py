36263ce8368e97af38ca0c3fb43bce41b24cb204
sagagouthamsai <sagagouthamsai@gmail.com>
2026-04-25 21:55:37 +0530

    Added decorators problem 10

---
+++ day45/patterns.py
@@ -0,0 +1,21 @@
+n=5
+for i in range(n+1,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+for i in range(n+1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+print("\n")
+
+for i in range(n+1):
+    for j in range(n+1):
+        print("*",end=' ')
+    print("")
+
+for i in range(n+1):
+    star="*"*(2*i-1)
+    print(star.center(2*n))
