607cedd9cc4dd2cc9b358bca5caeb927e413af63
sagagouthamsai <sagagouthamsai@gmail.com>
2026-05-25 23:28:21 +0530

    Added oops concept

---
+++ day46/opps.py
@@ -0,0 +1,18 @@
+#opps is an concept used in python using which we organize the code in such a manner that we reuse, organize and add modularity to the code.
+#the main cocepts of opps are 
+#encapsulation
+#inheritance
+#adstraction
+#polymorphism
+
+#Encapsulation
+"""
+its the process of combining class and objects together, restricting the access control
+consists of seter,getter methods to access and modify the contents
+for ACCESS CONTROL it has
+public var
+protected var (_var_name)
+private var (__var_name)
+"""
+
+
