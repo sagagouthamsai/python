6e874e3ec84105d142f32a5ea0dd5cfb7513092d
sagagouthamsai <sagagouthamsai@gmail.com>
2026-05-28 21:07:05 +0530

    Added encapsulation concept

--- day46/encapsulation.py
+++ day46/encapsulation.py
@@ -1,4 +1,4 @@
-class Hello:
+"""class Hello:
     def __init__(self, name, age, clg):
         self.name = name #public variavle
         self._age = age #protected variable
@@ -22,3 +22,23 @@ print(h.get_clg())
 #public variable : can be acessed from anywhere and is visible to any one
 #protected variable : can be accessed from anywhere but is not recomended as it is crutial in code 
 #private variable : can only be accessed inside the code block but is not visible to anyone
+"""
+
+def runningSum(nums=[]):
+    l=len(nums)
+    ans=[]
+    for i in range(1,l+1):
+        ans.append(sum(nums[:i]))
+    print(ans)
+
+
+def runningSum1(nums=list[int]):
+    l=len(nums)
+    ans=[]
+    count=0
+    for i in nums:
+        count+=i
+        ans.append(count)
+    print(ans)
+
+runningSum1([1,1,1,1,1])
