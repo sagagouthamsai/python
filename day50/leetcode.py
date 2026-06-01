994a2d5c1cf420a33c5695e5a8febf5c8e06d275
sagagouthamsai <sagagouthamsai@gmail.com>
2026-06-01 09:26:42 +0530

    Added leetcode problem

---
+++ day50/leetcode.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+            x=0
+            y=0
+            for i in moves :
+                if i == "U" :
+                    y+=1
+                elif i == "D" :
+                    y-=1
+                elif i == "R" :
+                    x+=1
+                elif i == "L" :
+                    x-=1
+            return x==y==0
+
+                
