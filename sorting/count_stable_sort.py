1ac9bf34d8a3100722fbbc03bd74f293ac1e11ef
sagagouthamsai <sagagouthamsai@gmail.com>
2026-07-09 20:40:56 +0530

    Added stable counting sort practice

---
+++ sorting/count_stable_sort.py
@@ -0,0 +1,19 @@
+def stbcnt(arr):
+    m=max(arr)+1
+    cnt=[0]*m
+    res=[0]*len(arr)
+    
+    for i in arr:  #freq count
+        cnt[i]+=1
+    
+    for i in range(1,m):  #prefix sum
+        cnt[i]+=cnt[i-1]
+        
+    for i in arr:  #travesing
+        res[cnt[i]-1]=i
+        cnt[i]-=1
+    return res
+
+
+arr = [4,2,2,8,3,3,1]
+print(stbcnt(arr))
