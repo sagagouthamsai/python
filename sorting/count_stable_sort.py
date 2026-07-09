f372707d05893729d0675dc1be09125de4d33460
sagagouthamsai <sagagouthamsai@gmail.com>
2026-07-09 20:49:32 +0530

    Added stable counting sort practice

--- sorting/count_stable_sort.py
+++ sorting/count_stable_sort.py
@@ -9,7 +9,7 @@ def stbcnt(arr):
     for i in range(1,m):  #prefix sum
         cnt[i]+=cnt[i-1]
         
-    for i in reversed(arr):  #travesing
+    for i in reversed(arr):  #travesing or we can even use len(arr])-1,-1,-1
         res[cnt[i]-1]=i
         cnt[i]-=1
     return res
