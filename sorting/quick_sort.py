770d7c3968ad0b0b985088ce1ceec18597c00d0e
sagagouthamsai <sagagouthamsai@gmail.com>
2026-07-16 21:03:59 +0530

    Added quick sort practice

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,4 +1,4 @@
-def qs(arr,low,high):
+def par(arr,low,high):
     pivot = arr[high]
     i = low - 1
     for j in range(low, high):
@@ -9,8 +9,15 @@ def qs(arr,low,high):
 
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
 
-    return arr
+    return i+1
 
+def qs(arr,low,high):
+
+    if low<high:
+        p=par(arr,low,high)
+        p1=qs(arr,low,p-1)
+        pr=qs(arr,p+1,high)
+    return arr
 
 arr=[1,99,999,3,4,2,1,111,2,3,66,5,44,111,1]
 print(qs(arr,0,len(arr)-1))
