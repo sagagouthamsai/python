caf43dd8691a71dc3fbcb996cb7936fb88417f04
sagagouthamsai <sagagouthamsai@gmail.com>
2026-07-22 23:39:14 +0530

    Added two pointers practice

--- twopointers/tp2.py
+++ twopointers/tp2.py
@@ -14,3 +14,24 @@ def twoSum( numbers, target) :
 
 
 print(twoSum(numbers=[2,7,11,15],target=9))
+
+
+def is_prime(n):
+    if n<=1:
+        return []
+    elif n==2:
+        return [2]
+    r=[2]
+    s=3
+    while s<n:
+        c=True
+        for i in range(2,int(s**0.5)+1,2):
+            if s%i==0:
+                c=False
+                break
+        if c:
+            r.append(s)
+        s+=2
+    return r
+print(is_prime(30))
+             
