5f2447a4a1b444b023e586d8b502d29416732c7b
sagagouthamsai <sagagouthamsai@gmail.com>
2026-07-30 22:42:53 +0530

    Added leetcode problem no 3

--- leetcode/3.py
+++ leetcode/3.py
@@ -17,3 +17,23 @@ class Solution:
 
         max_sub=max(max_sub,len(p))
         return max_sub
+
+
+#optimized version
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        p = 0
+        r = {}
+        max_sub = 0
+
+        if not s:
+            return 0
+
+        for i in range(len(s)):
+            if s[i] in r and p <= r[s[i]]:
+                p = r[s[i]] + 1
+
+            r[s[i]] = i
+            max_sub = max(max_sub, i - p + 1)
+
+        return max_sub
