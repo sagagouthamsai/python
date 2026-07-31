77caf3bd88876e23d7b240f9f6d02b05f3cefcd3
sagagouthamsai <sagagouthamsai@gmail.com>
2026-07-31 23:32:38 +0530

    Added leetcode problem no 209

---
+++ leetcode/209.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        b_m=float('inf')
+        tot=0
+        l=0
+        for i in range(len(nums)):
+            tot+=nums[i]
+            while tot>=target:
+                b_m=min(b_m,i-l+1)
+                tot-=nums[l]
+                l+=1
+        return b_m if b_m!=float("inf") else 0
