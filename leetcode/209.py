76af94cdcacd022802e92cba9602cc4da15814ce
sagagouthamsai <sagagouthamsai@gmail.com>
2026-07-31 23:33:42 +0530

    Added leetcode problem no 209

--- leetcode/209.py
+++ leetcode/209.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+    def minSubArrayLen(self, target: int, nums: list()) -> int:
         b_m=float('inf')
         tot=0
         l=0
