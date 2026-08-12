49d2de2109baa199c4c53b77e1b2de1de6734c3d
sagagouthamsai <sagagouthamsai@gmail.com>
2026-08-12 21:40:07 +0530

    Added linked list basic operations

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -30,8 +30,6 @@ class linked_list:
         
         self.head=self.head.next
 
-
-
     def show(self):
         temp=self.head
         if temp==None:
@@ -45,7 +43,7 @@ class linked_list:
             temp=temp.next
         print("\n_______________________________________________________")
 
-
+"""
 new=linked_list()
 new.show()
 new.insert_at_end(1)
@@ -63,6 +61,87 @@ new.show()
 new.insert_at_end(1)
 new.show()
 new.insert_at_begining(0)
-new.show()
+new.show()"""
 
 
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.prev=None
+
+class ll:
+    def __init__(self):
+        self.head=None
+
+    def insert_head(self,data):
+        new_node=node(data)
+
+        if self.head:
+            self.head.prev=new_node
+
+        new_node.next=self.head
+        self.head=new_node
+
+
+    def insert_tail(self,data):
+        new=node(data) 
+        if self.head==None:
+            self.head=new
+            return
+        temp=self.head
+        while temp.next:
+            temp=temp.next
+        temp.next=new
+        new.prev=temp
+
+    def show_prev_next(self):
+        current = self.head
+
+        print("__________________________________________________________________")
+        while current:
+            prev_value = current.prev.data if current.prev else None
+            next_value = current.next.data if current.next else None
+
+            print("Prev:", prev_value,"| Data:", current.data,"| Next:", next_value)
+            current = current.next
+        print("__________________________________________________________________")
+
+n1=ll()
+n1.insert_head(1)
+n1.insert_head(9)
+n1.insert_head(10)
+n1.insert_head(1000)
+n1.insert_head(100)
+n1.insert_tail(1000)
+n1.insert_tail(29)
+n1.insert_tail(71)
+#n1.show_prev_next()
+
+"""
+cur=int(input("Enter the Year you wish to start : "))
+n=int(input("Enter how many years you want : "))
+res=0
+while res<=n:
+    if cur%4==0:
+        print("Count :",res,"    ","Year :  ",cur)
+        res+=1
+    cur-=1
+
+    if cur<0:
+        print("Hit the limit")
+        break"""
+
+"""
+n=int(input("Enter the number : "))
+bin=""
+
+if not n:
+    print(n)
+
+while n:
+    bin+=str(n%2)
+    n//=2
+
+print(bin[::-1])
+print(int("111011101101010111010111",2))"""
